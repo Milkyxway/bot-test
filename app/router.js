9873d798c021206304abfe9119ff202e2ab0d3c5
xjs <xjsww@1214@163.com>
2024-04-30 14:12:53 +0800

    fix: fix many code

--- app/router.js
+++ app/router.js
@@ -1,10 +1,11 @@
 /**
  * @param {Egg.Application} app - egg application
  */
-module.exports = app => {
+module.exports = async app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   const { Client } = require('amesu');
+  let token;
   const client = new Client({
     appid: '102099031',
     token: 'JC3MBrMGfLT1ORbCfDPFzuOXOEgbuv8n',
@@ -13,20 +14,27 @@ module.exports = app => {
     sandbox: true,
     log_level: 'ALL',
   });
-  const commonCall = (url, method) => {
+
+  /**
+   * 通用的api请求
+   * @param {*} url
+   * @param {*} method
+   * @param {*} noToken
+   * @return
+   */
+  const commonCall = (url, method, noToken = false) => {
     return new Promise((resolve, reject) => {
       try {
         fetch(url, {
           method,
-          headers: {
+          headers: noToken ? { 'Content-Type': 'application/json' } : {
             'Content-Type': 'application/json',
-            Authorization: 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjZXNoaTA2IiwidXNlcklkIjoiMTU3NjQ3MzczMDI5NjM1IiwibmFtZSI6IuS4geeQrueQriIsImV4cCI6MTcxNDQ3NTUxM30.lx-PFlGFTLrr3IqO3Nn9-J9RUkfgDp_r9B0HYq1YST1JYCd-Zb3OWMPtazkSPi9QGWniM0nKbD8xzYzINmJfEQiR5dL0a87t58tOPYrp8nujkNHVuvJ2UDqS5f_ovFhdnGu0kGPmY5cNRgoyDBK81MvQ6wiLoYCD1TzQ_3Jk0wE',
+            Authorization: token,
           },
           mode: 'no-cors', // 设置cors表示只能发送跨域的请求，no-cors表示跨不跨域都能发
         }).then(res => {
           return res.json();
         }).then(async data => {
-
           resolve(data.data);
         });
       } catch (e) {
@@ -37,10 +45,28 @@ module.exports = app => {
 
   };
 
-  const login = () => {
+  /**
+   * 获取最新token
+   * @return
+   */
+  const getLastestToken = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await commonCall('http://172.16.251.75:8090/nms/api/admin/login?loginName=ceshi06&pwd=023e1e2ab4a2746d28cd3688e867825f', 'post', true);
+        token = result.token;
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    });
 
   };
 
+  /**
+   * 通用机器人发送群聊消息
+   * @param {*} qqEvent
+   * @param {*} content
+   */
   const commonSendGroupMsg = async (qqEvent, content) => {
     await client.api.sendGroupMessage(qqEvent.group_openid, {
       msg_id: qqEvent.id,
@@ -49,9 +75,13 @@ module.exports = app => {
     });
   };
 
+  /**
+   * 区分指令调用不同接口
+   * @param {*} qqEvent
+   */
   const distinguishApi = async qqEvent => {
     if (qqEvent.content.indexOf('/宽带账号认证') > -1) {
-      const account = qqEvent.content.split('认证')[1];
+      const account = qqEvent.content.split('认证')[1].replace(/\s*/g, '');
       const url = `http://172.16.251.75:8090/nms/api/fttx/aaa/aaaCertification?accessUserName=${account}`;
       const result = await commonCall(url, 'get');
       commonSendGroupMsg(qqEvent, `\n账号${account}\n宽带账号认证结果\n认证状态: ${result.isPass}\n在线状态:${result.onlineStatus}\n认证服务器: ${result.nickName}\n认证服务器ip: ${result.ip}`);
@@ -65,11 +95,7 @@ module.exports = app => {
       const ccNameInfo = await commonCall(`http://172.16.251.75:8090/nms/api/fttx/onu/getCCname?mac=${account}`, 'get');
       commonSendGroupMsg(qqEvent, `\n账号${account}\nCM/ONU快速查询结果\n终端类型: ${onuInfo.onuType}\n机房:${onuInfo.roomName}\n所属头端: ${onuInfo.epon}\n站点:${onuInfo.departName}\n在线状态:${onuInfo.status === 1 ? '在线' : '未知'}\n光功率信号:${detailInfo.onuReceivePower}\n业务类型:${ccNameInfo.ccname}`);
     } else {
-      await client.api.sendGroupMessage(qqEvent.group_openid, {
-        msg_id: qqEvent.id,
-        msg_type: 0,
-        content: qqEvent.content,
-      });
+      commonSendGroupMsg(qqEvent, qqEvent.content);
     }
 
   };
@@ -89,6 +115,10 @@ module.exports = app => {
 
   });
 
+  await getLastestToken();
+
   // 机器人上线
   client.online();
+
+
 };
